62739fdb35f22daeaf60497fd179942e2d2f9861
Joseph Handschu <jhandschu@gmail.com>
2017-09-16 10:51:14 -0400

    Adding main feed

--- index.php
+++ index.php
@@ -11,6 +11,25 @@ require 'includes/header.php';
 		<a href="#">
 			<img src="<? echo $user['profile_pic'] ?>">
 		</a>
+		<div class="user_details_left_right">
+			<!-- User Details Data -->
+			<a href="#">
+			<?
+				echo $user['first_name'] . "<br>"; // . $user['last_name'];
+			?>
+			</a>
+			<?
+				echo "Posts: " . $user['num_posts'] . "<br>" ;
+				echo "Likes: " . $user['num_likes'];
+			?>
+		</div>
+	</div>
+
+	<div class="main_column column">
+		<form class="post_form" action="index.php" method="POST"></form>
+			<textarea name="post_text"></textarea>
+
+
 	</div>
 
 
