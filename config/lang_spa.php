6c7009984eff13e4ccf2778f6fd1bb75b64657dc
Joseph Handschu <jhandschu@gmail.com>
2017-10-14 17:12:10 -0400

    Language Handling
    Basic language handler

--- config/lang_spa.php
+++ config/lang_spa.php
@@ -3,8 +3,8 @@ $xmlstr = <<<XML
 <?xml version="1.0" encoding="UTF-8"?>
 <langugefile>
 	<language value="ENG" DESC="English - US">
-		<key value="lbl_post_1">Posts</key>			
-		<key value="lbl_like_1">Like</key>			
+		<key value="lbl_post_1">Publicar</key>			
+		<key value="lbl_like_1">Me Gusta</key>			
 		<key value="lbl_menu_1">menu1</key>			
 		<key value="lbl_menu_2">menu2</key>			
 		<key value="lbl_meni_3">menu3</key>			
---
+++ includes/language_handler.php
@@ -0,0 +1,14 @@
+<?php 
+	
+$lang = "spa";
+
+if ($lang == "eng")
+	include("../config/lang_eng.php");
+else
+	include("../config/lang_spa.php");
+
+var_dump($xmlstr);
+
+
+
+ ?>
