a7abeabb7a365c59d222d8cf1a4da45014b47ffc
Joseph Handschu <jhandschu@gmail.com>
2017-10-25 22:30:31 -0400

    User Messaging
    Work in progress

--- assets/classes/Message.php
+++ assets/classes/Message.php
@@ -20,21 +20,21 @@ class Message
 		$most_recent = false;
 
 		// find who received or send the most recent message
-		$message_qry = mysqli_query($this->conn , 
+		$msg_qry = mysqli_query($this->conn , 
 			"SELECT msg_user_to, msg_user_from 
 			 FROM soc_messages
 			 WHERE msg_user_to = '$loggedInUser' 
 			 OR msg_user_from = '$loggedInUser'
 			 ORDER BY id DESC 
 			 LIMIT 1");
-		if (mysqli_num_rows($message_qry) == 0)
+		if (mysqli_num_rows($msg_qry) == 0)
 			$most_recent = false;
 		else 
 		{
-			$row = mysqli_fetch_array($message_qry);
+			$row = mysqli_fetch_array($msg_qry);
 
-			$user_to = $row['user_to'];
-			$user_from = $row['user_from'];
+			$user_to = $row['msg_user_to'];
+			$user_from = $row['msg_user_from'];
 
 			if ($user_to != $loggedInUser)
 				$most_recent = $user_to;
@@ -42,6 +42,32 @@ class Message
 				$most_recent = $user_from;	
 		}
 		return $most_recent;
+	}
+
+	public function sendMessage($user, $msg, $date)
+	{
+		if ($msg != '')
+		{
+			$loggedInUser = $this->user_obj->getUsername();
+			$msg_qry = mysqli_query($this->conn ,
+				"INSERT INTO soc_messages(id, msg_user_to, msg_user_from, msg_body, 
+					msg_date, msg_opened, msg_viewed, msg_deleted)
+			    VALUES('','$user','$loggedInUser','$msg','$date','no','no','no')");
+		}
+	}
+
+	public function getMessages($user)
+	{
+		$msg_qry = mysqli_query($this->conn ,
+			"SELECT * 
+			 FROM soc_messages
+			 WHERE msg_user_to = '$user' AND msg_deleted = 'no'");
+		if (mysqli_num_rows($msg_qry) > 0)
+			$messages = mysqli_fetch_all($msg_qry, MYSQL_ASSOC);
+		else
+			$messages = "none";
+
+		return $messages;
 	}
 }
 
--- messages.php
+++ messages.php
@@ -17,9 +17,20 @@ else
 }
 if ($user_to != 'new')
 	$user_to_obj = new User ($con , $user_to);
+if (isset($_POST['post_msg']))
+{
+	if (isset($_POST['msg_body']))
+	{
+		$check_safe = $U->stringSafe($con , $_POST['msg_body']);
+		$body = $check_safe;
+
+		$date = date("Y-m-d H:i:s");
+		$message_obj->sendMessage($user_to,$body,$date);
+	}
+}
 
 
- ?>
+?>
 
 <div class="user_details column">
 	<a href="<? echo $user['username']; ?>">
@@ -33,40 +44,65 @@ if ($user_to != 'new')
 		?>
 		</a>
 		<?
-			// lbl_post_1
-			// getLangKey("lbl_post_1");
 			echo $U->getLanguageKey("key_lbl_post_1",$lang). ":"  . $user['num_posts'] . "<br>" ;
 			echo $U->getLanguageKey("key_lbl_like_1",$lang). ":"  . $user['num_likes'];
 		?>
 	</div>
 </div>
 <div class="main_column column" id="main_column">
+<?
 
-</div>
+if ($user_to != 'new')
 
-<div class="loaded_messages column">
+	echo "<h4>You and <a href='$user_to'>" .$user_to_obj->getFirstAndLastName() ."</a></h4><hr><br>";
+?>
+<div class="post_message">
 	<form action="" method="POST">
 	<?
-	if($user_to = 'new')
+	if($user_to == "new")
 	{
 		echo "Select the user you would like to send a message<br><br>";
 		echo "To: <input type='text'";
 		echo "<div class='message' id='results'></div>";
 	}
-
-
-
-
+	else
+	{
+		echo "<textarea name='msg_body' id='msg_textarea' placeholder='Type your message..'></textarea>";
+		echo "<input type='submit' class='info' name='post_msg' id='msg_submit' value='Send Message'>";
+	}
 	?>
-
-
-
-
-
 	</form>
-
-
-
-
+</div>
+<?
+	echo "<div class='loaded_messages'>";
+	$messages = $message_obj->getMessages($user_to);
+	?>
+	<div class="container">
+		<table class="table">
+			<thead>
+				<tr>
+				<!--	<th>Message From</th> -->
+					<th>Message</th>
+					<th>Date</th>
+					<th>Delete</th>
+				</tr>
+			</thead>
+			<tbody>
+	<?
+	foreach ($messages as $row) 
+	{
+		echo "<tr>";
+		//echo "<td>" . $row['msg_user_from'] . "</td>";
+		echo "<td>" . $row['msg_body'] . "</td>"; 
+		echo "<td>" . $row['msg_date'] . "</td>";
+		echo "<td><a href='#'>Delete</a></td>";
+		echo "</tr>";
+	}
+	//print_r ($message_obj->getMessages($user_to));
+?>
+			</tbody>
+		</table>
+	</div>
+</div>
 
 </div>
